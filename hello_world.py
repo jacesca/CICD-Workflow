d3a620094495ddcf902006d106f2d5c6de13dabb
jacesca <jacesca @gmail.com>
2024-08-29 11:36:14 -0600

    Changing import in hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,11 @@
-import datetime
+from datetime import datetime
 
 
 def main():
     print("Hello, World!")
 
     # Get the current time and print it
-    current_time = datetime.datetime.now()
+    current_time = datetime.now()
     print("Current time:", current_time)
 
 
